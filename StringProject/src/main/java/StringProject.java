05988ee1435c201b29739c889148959163bdbadf
MSTFTKR <125224568+MSTFTKR@users.noreply.github.com>
2023-08-11 22:02:57 +0300

    nextt

---
+++ StringProject/src/main/java/StringProject.java
@@ -0,0 +1,42 @@
+
+public class StringProject {
+
+    public static void main(String[] args) {
+        
+      String a="MUstafa";
+      String b = new String("Mustafa");
+      
+        System.out.println(b);
+        
+        /*System.out.println("Harf Sayısı: "+b.length());
+        
+        System.out.println("0.İndeks:"+b.charAt(0)); //0.indekcste ki harf
+        System.out.println("Son İndeks:"+b.charAt(b.length()-1)); */
+        
+        /*for (int i = 0; i < b.length(); i++) {
+            System.out.println(b.charAt(i));
+                    
+            
+        }*/
+        
+        /*System.out.println(b.startsWith("Mu")); //Mu ile başlıyor mu ? true false
+        System.out.println(b.endsWith("af"));*/
+        
+        /*System.out.println(b.indexOf("a")); //kaçıncı indexte baştan alır
+        System.out.println(b.lastIndexOf("a"));*/
+        
+        /*System.out.println(b.toLowerCase()); //küçük büyük harf
+        System.out.println(b.toUpperCase());*/
+        
+        /*String c="1923";
+        int d=Integer.parseInt(c); // yazıyı intigera çevirme
+        System.out.println(d-10);*/
+        
+//        int x=15;
+//        String y=String.valueOf(x); //integer stringe çevirme
+//        System.out.println(y+15);
+
+            
+        
+    }
+}
